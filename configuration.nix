b82786e6ec933d0a949d131653aa530c482fc68f
Kevin King <kcking@google.com>
2023-09-30 09:50:14 +0000

    i3status-rs, neovim, etc.

--- configuration.nix
+++ configuration.nix
@@ -4,12 +4,42 @@
 
 { config, lib, pkgs, ... }:
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./apple-silicon-support
-    ];
+let
+  i3statusbarconfig = pkgs.writeText "i3statusbar-config" ''
+    [theme]
+    theme = "gruvbox-dark"
+    [icons.overrides]
+    bat = [
+        "| |",
+        "|¼|",
+        "|½|",
+        "|¾|",
+        "|X|",
+    ]
+    bat_charging = "|^|"
+    [[block]]
+    block = "battery"
+    driver = "upower"
+    empty_format = " $icon $percentage $time "
+    format = " $icon $percentage $time "
+    [[block]]
+    block = "cpu"
+    interval = 1
+    [[block]]
+    block = "uptime"
+    [[block]]
+    block = "sound"
+    [[block]]
+    block = "time"
+    interval = 10
+    format = "$timestamp.datetime()"
+  '';
+
+in {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./apple-silicon-support
+  ];
   hardware.asahi.extractPeripheralFirmware = true;
   hardware.asahi.peripheralFirmwareDirectory = ./firmware;
   hardware.opengl.enable = true;
@@ -23,7 +53,8 @@
   networking.hostName = "sentrynixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable =
+    true; # Easiest to use and most distros use this by default.
   nixpkgs.config.allowUnfree = true;
 
   # Set your time zone.
@@ -49,9 +80,6 @@
     Defaults        timestamp_timeout=300
   '';
 
-
-  
-
   # Configure keymap in X11
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e,caps:escape";
@@ -70,110 +98,118 @@
   users.users.kevin = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      firefox
-      tree
-    ];
+    packages = with pkgs; [ firefox tree ];
     shell = pkgs.zsh;
   };
-home-manager.users.kevin = { pkgs, ... }: {
-  home.packages = with pkgs; [ 
-    chromium
-    fuzzel
-    wezterm
- ];
-  home.stateVersion = "23.11";
-  wayland.windowManager.sway = {
-    enable = true;
-    systemd.enable = true;
-    config = rec {
-      # terminal = "wezterm";
-      modifier = "Mod4";
-      input = {
-        "*" = {
-		xkb_options = "caps:escape";
-		repeat_delay = "180";
-        };
-      };
-      output = {
-        "*" = {
-          scale = "2";
-        };
-      };
-    };
-  };
-  home.shellAliases = {
-    g = "git";
-    "..." = "cd ../..";
-    nxx = "sudo vi /etc/nixos/configuration.nix";
-    nrb = "sudo nixos-rebuild switch --flake /etc/nixos/#";
-  };
-# https://www.reddit.com/r/NixOS/comments/nxnswt/cant_change_themes_on_wayland/h1skv8w/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-# try to fix wezterm cursor issues
-   gtk.enable = true;
-   gtk.font.name = "Noto Sans";
-   gtk.font.package = pkgs.noto-fonts;
-   gtk.theme.name = "Dracula";
-   gtk.theme.package = pkgs.dracula-theme;
-   gtk.iconTheme.name = "Papirus-Dark-Maia";  # Candy and Tela also look good
-   gtk.iconTheme.package = pkgs.papirus-maia-icon-theme;
-   gtk.cursorTheme.name = "Capitaine Cursors";
-   gtk.cursorTheme.package = pkgs.capitaine-cursors;
-   gtk.gtk3.extraConfig = {
-     gtk-application-prefer-dark-theme = true;
-     gtk-key-theme-name    = "Emacs";
-     gtk-icon-theme-name   = "Papirus-Dark-Maia";
-     gtk-cursor-theme-name = "capitaine-cursors";
-   };
-   dconf.settings = {
-     "org/gnome/desktop/interface" = {
-       gtk-key-theme = "Emacs";
-       cursor-theme = "Capitaine Cursors";
-     };
-   };
-   xdg.systemDirs.data = [
-     "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
-     "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
-   ];
-
-  programs.zsh = {
-    enable = true;
-    oh-my-zsh = {
+  home-manager.users.kevin = { pkgs, ... }: {
+    home.packages = with pkgs; [ chromium fuzzel wezterm nixfmt ];
+    home.stateVersion = "23.11";
+    wayland.windowManager.sway = {
       enable = true;
-      plugins = [ "git" "sudo" ];
-      theme = "robbyrussell";
+      systemd.enable = true;
+      config = rec {
+        # terminal = "wezterm";
+        modifier = "Mod4";
+        input = {
+          "*" = {
+            xkb_options = "caps:escape";
+            repeat_delay = "180";
+          };
+        };
+        output = { "*" = { scale = "2"; }; };
+        bars = [{
+          statusCommand =
+            "${pkgs.i3status-rust}/bin/i3status-rs ${i3statusbarconfig}";
+        }];
+      };
     };
+    home.shellAliases = {
+      g = "git";
+      "..." = "cd ../..";
+      nxx = "sudo vi /etc/nixos/configuration.nix";
+      nrb = "sudo nixos-rebuild switch --flake /etc/nixos/#";
+    };
+    # https://www.reddit.com/r/NixOS/comments/nxnswt/cant_change_themes_on_wayland/h1skv8w/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    # try to fix wezterm cursor issues
+    gtk.enable = true;
+    gtk.font.name = "Noto Sans";
+    gtk.font.package = pkgs.noto-fonts;
+    gtk.theme.name = "Dracula";
+    gtk.theme.package = pkgs.dracula-theme;
+    gtk.iconTheme.name = "Papirus-Dark-Maia"; # Candy and Tela also look good
+    gtk.iconTheme.package = pkgs.papirus-maia-icon-theme;
+    gtk.cursorTheme.name = "Capitaine Cursors";
+    gtk.cursorTheme.package = pkgs.capitaine-cursors;
+    gtk.gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-key-theme-name = "Emacs";
+      gtk-icon-theme-name = "Papirus-Dark-Maia";
+      gtk-cursor-theme-name = "capitaine-cursors";
+    };
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        gtk-key-theme = "Emacs";
+        cursor-theme = "Capitaine Cursors";
+      };
+    };
+    xdg.systemDirs.data = [
+      "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    ];
+
+    programs.zsh = {
+      enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "sudo" ];
+        theme = "robbyrussell";
+      };
+    };
+
+    home.sessionVariables = { XCURSOR_THEME = "Adwaita"; };
+
+    programs.i3status-rust = { enable = true; };
+
+    programs.vscode = { enable = true; };
+
+    programs.neovim = {
+      defaultEditor = true;
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+
+      plugins = with pkgs.vimPlugins; [
+        nvim-lspconfig
+        nvim-treesitter.withAllGrammars
+        plenary-nvim
+        gruvbox-material
+        mini-nvim
+        neoformat
+      ];
+    };
+
   };
 
-home.sessionVariables = {
-XCURSOR_THEME = "Adwaita";
-};
+  programs.zsh.enable = true;
 
-};
-
-programs.zsh.enable = true;
-
-environment.loginShellInit = ''
+  environment.loginShellInit = ''
     [[ "$(tty)" == /dev/tty1 ]] && sway
   '';
 
-# chromium wayland support, fixes high-dpi scaling
-environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
+  # chromium wayland support, fixes high-dpi scaling
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    vscode
     git
     bottom
   ];
 
-# services.xserver.enable = true;
-services.xserver.displayManager.gdm.enable = true;
-services.xserver.desktopManager.gnome.enable = true;
-
+  # services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   programs._1password.enable = true;
   programs._1password-gui = {
